86caddd55232f19b684b3d3cf60a851566f6c405
Nicolas Aguilar <aguilarnicolasivan@gmail.com>
2023-02-16 23:19:12 -0300

    carousel with swiper

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import Home from "./Components/Home/index.jsx";
-import "./App.css";
-
-function App() {
-  return <Home />;
-}
-
-export default App;
--- src/Pages/Home/Portfolio/index.jsx
+++ src/Pages/Home/Portfolio/index.jsx
@@ -1,42 +1,70 @@
-import "./Porfolio.css";
 import imagenTres from "./assets/th.jpg";
 import imagenDos from "./assets/b5ed810a-4e20-46a5-a278-50ece165bde9.jpg";
 import imagenUno from "./assets/pasos-para-desarrollar-tu-pagina-web.jpg";
 import imagenCuatro from "./assets/maxresdefault.jpg";
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { useState } from "react";
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 function NoTransitionExample() {
+
+  const swiper = useSwiper()
+
+  const [swiperRef, setSwiperRef] = useState(null)
+  const [index, setIndex] = useState(0)
+
+  const handleChangeIndex = (type) => {
+    if (type === 'next') {
+
+    }
+
+    if (type === 'previous') {
+
+    }
+  }
+
+
   return (
-    <div id="carouselExample" className="carousel slide">
-      <div className="carousel-inner">
-        <div className="carousel-item" style={{ display: "flex" }}>
-          <img src={imagenTres} className="d-block w-100" alt="..." />
-          <img src={imagenDos} className="d-block w-100" alt="..." />
-          <img src={imagenUno} className="d-block w-100" alt="..." />
-          <img src={imagenCuatro} className="d-block w-100" alt="..." />
-        </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev"
-          style={{position: 'relative', top: '-10px', left: '500px'}}
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+    <div>
+      <Swiper
+        onSwiper={setSwiperRef}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={2}
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSlideChange={(e) => console.log(e)}
+      >
+        <SwiperSlide>
+          <img src={imagenUno} alt="..." />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={imagenDos} alt="..." />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={imagenTres} alt="..." />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={imagenCuatro} alt="..." />
+        </SwiperSlide>
+      </Swiper>
+      <p>
+        <button onClick={() => handleChangeIndex('previous')} className="prepend-2-slides">
+          Anterior
         </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next"
-          style={{position: 'relative', top: '-10px', left: '600px'}}
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+        <button onClick={() => handleChangeIndex('next')} className="prepend-slide">
+          Siguiente
         </button>
-      </div>
+      </p>
     </div>
   );
 }
 
 export default NoTransitionExample;
+
