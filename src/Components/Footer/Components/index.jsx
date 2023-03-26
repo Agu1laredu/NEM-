c7da904f1196942b7013cd7aa24d84e0dccb6651
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-03-25 21:13:01 -0300

    fix

--- src/Components/Footer/Components/index.jsx
+++ src/Components/Footer/Components/index.jsx
@@ -3,33 +3,32 @@ import { AiFillInstagram } from "react-icons/ai";
 import styled from "styled-components";
 
 function Footer2() {
-    const Footer = styled.div`
-      display: flex;
-      justify-content: space-between;
-      height: 8vh;
-      align-items: center;
-      & p {
-        width: 33vw;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-      }
-    `
-    const FontIcon = styled.div`
-      font-size: x-large;
-      display: flex;
-      justify-content: space-around;
+  const Footer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 8vh;
+    align-items: center;
+    & p {
       width: 33vw;
-    `
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  `;
+  const FontIcon = styled.div`
+    font-size: x-large;
+    display: flex;
+    justify-content: space-around;
+    width: 33vw;
+  `;
   return (
-
     <Footer>
       <p>@NEM TECHNOLOGIES 2023</p>
 
       <FontIcon>
-        <BsTwitter/>
-        <BsLinkedin/>
-        <AiFillInstagram/>
+        <BsTwitter style={{ cursor: "pointer" }} />
+        <BsLinkedin style={{ cursor: "pointer" }} />
+        <AiFillInstagram style={{ cursor: "pointer" }} />
       </FontIcon>
     </Footer>
   );
